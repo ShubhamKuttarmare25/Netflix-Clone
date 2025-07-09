ac58981be18070a847aceec250bc7ef10f57b1e1
Shubham Kuttarmare <shubhamkuttarmare8329@gmail.com>
2025-07-09 13:50:43 +0530

    before authentication

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png';
+import { useNavigate, useParams } from 'react-router-dom';
 const Player = () => {
+
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name:"", 
+    key: "",
+    published_at:"",
+    typeof:""
+  })
+
+      const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMmNlNDZkNmZhZmE5ZGU3YWIzYzk5YTQwZTg0MzcyNyIsIm5iZiI6MTc1MTI4Mzk4OC40NDcsInN1YiI6IjY4NjI3OTE0ZWE1Y2I0NmQ2N2E0ZDk5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lumVno_jgCX6xNYBi38ZyjAEotVBmhK0mPxp0MHE1ZI'
+      }
+    };
+
+  useEffect(()=>{
+      fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+      .then(res => res.json())
+      .then(res => setApiData(res.results[0]))
+      .catch(err => console.error(err));
+
+  },[])
+
+
   return (
     <div className='player'>
 
+      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}} />
+      <iframe width='90%' height='90%'
+      src={`https://www.youtube.com/embed/${apiData.key}`} title='Trailer' frameBorder='0' allowFullScreen></iframe>
+
+      <div className='player-info'>
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
     </div>
   )
 }
